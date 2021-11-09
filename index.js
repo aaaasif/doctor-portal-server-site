899195e1badeb2a146b7664917a79680dc1346f8
aaaasif <alexasif4@gmail.com>
2021-11-09 17:22:45 +0600

    error handle

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ async function run() {
 
         app.get('/appointments', verifyToken, async (req, res) => {
             const email = req.query.email;
-            const date = new Date(req.query.date).toLocaleDateString();
+            const date = req.query.date;
             // console.log(date);
             const query = { email: email, date: date }
             // console.log(query);
